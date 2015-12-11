2c79f83d0e6fa773a0e170d8fc9ad2ac956da0c6
slaveunit <marco.abundo@live.com>
2015-12-11 21:55:01 +0000

    Got to get Paperclip to work.  Deleted paperclipoptions in config/envi/development.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,11 @@
 class Product < ActiveRecord::Base
 
+# PAPERCLIP
+has_attached_file :photo, default_url: "/images/:style/missing.png"
 
-	# PAPERCLIP
-	has_attached_file :photo, default_url: "/images/:style/missing.png"
-
-	validates_attachment :photo,
-	:presence => true,
-	:size => { :in => 0..10.megabytes },
-	:content_type => { :content_type => /^image\/(jpg|jpeg|png|gif|tiff)$/ }
-	# PAPERCLIP
-
+validates_attachment :photo,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+# PAPERCLIP
 
 end
 
