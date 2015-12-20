91d61612a98aaf57a16a3d4f1e4929506973bb23
slaveunit <marco.abundo@live.com>
2015-12-20 15:33:03 +0000

    Paperclip Amazon.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
   #paperclip amazon aws
   config.paperclip_defaults = {
     :storage => :s3,
+    :region => 'us-west-2',
     :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => 'us-west-2',
     :s3_host_name => 's3-us-west-2.amazonaws.com',
     :url => ":s3_domain_url"
   }
