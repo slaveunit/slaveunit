f368b49b5d1ee2e852b3c3937ba5b4ebbc12463d
slaveunit <marco.abundo@live.com>
2015-12-20 15:52:58 +0000

    Paperclip Amazon.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :s3_endpoint => 's3-us-west-2.amazonaws.com',
     :url => ":s3_domain_url"
   }
 }
