d04d287fa5663afdb13d184e22c93ef629698f73
slaveunit <marco.abundo@live.com>
2015-12-20 05:33:09 +0000

    Attempt link Paperclip to Amazon AWS.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_region=> 'us-east'
+    :s3_region=> 'us-west-2'
   }
 }
 end
