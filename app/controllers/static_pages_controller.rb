dd9cd54e7329e99f4ccb2c30c8c934a1f45461fe
slaveunit <marco.abundo@live.com>
2015-12-27 20:30:15 +0000

    Made responsive.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   # for the CAROUSEL
   def index
     @products = Product.all
-    @featured_product = Product.first
+    @featured_product = Product.third
   end
 
   def store
