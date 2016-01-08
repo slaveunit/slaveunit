77050a6a773975a4076c917f1db185f3e346ba7b
slaveunit <marco.abundo@live.com>
2016-01-08 20:43:32 +0000

    Fixed Devise to record first_name and last_name when signing up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+# DEVISE Since devise default saves only email and password, add this block to add first_name and last_name.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :email, :password) }
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  end
+# DEVISE Since devise default saves only email and password, add this block to add first_name and last_name.
 
-rescue_from CanCan::AccessDenied do |exception|
+# CANCAN Directs page to root url when attempt access unauthorized page
+	rescue_from CanCan::AccessDenied do |exception|
 	redirect_to main_app.root_url, :alert => exception.message
+# CANCAN Directs page to root url when attempt access unauthorized page
+
 end
 
 end
