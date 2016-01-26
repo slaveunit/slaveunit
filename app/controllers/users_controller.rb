a3c429079bdfff23f124b2fed696860fe87eb88f
slaveunit <marco.abundo@live.com>
2016-01-26 03:04:13 +0000

    Sign-up email set-up. Sendgrid..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,10 +33,12 @@ load_and_authorize_resource
 
     respond_to do |format|
       if @user.save
+        
+# SENDGRID Deliver the signup email
+        UserNotifier.send_signup_email(@user).deliver  
+
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
-# SENDGRID Deliver the signup email
-        UserNotifier.send_signup_email(@user).deliver
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,13 +8,6 @@ class UserMailer < ApplicationMailer
 			:subject => "A new contact form message from #{name}")
 	end
 
-	  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_signup_email(user)
-    @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for checking out SLAVE UNIT APPAREL' )
-  end
-
   def thank_you
     @name = params[:name]
     @email = params[:email]
