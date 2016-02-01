d72edc1bab3512a6d79030df289295ef9bb0b0e5
slaveunit <marco.abundo@live.com>
2016-02-01 01:36:40 +0000

    Attempt make shopping cart.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,78 +37,80 @@ class ProductsController < ApplicationController
       end
   end
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
+  # GET /products/1
+  # GET /products/1.json
+  # PAGINATE
+  def show
+    @comments = @product.comments.order("created_at DESC").paginate(page: params[:page], per_page: 3)
+  end
 
-    # GET /products/1
-    # GET /products/1.json
-    # PAGINATE
-    def show
-      @product = Product.find(params[:product_id])
-      @comments = @product.comments.order("created_at DESC").paginate(page: params[:page], per_page: 3)
-    end
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
 
-    # GET /products/new
-    def new
-      @product = Product.new
-    end
+  # GET /products/1/edit
+  def edit
+  end
 
-    # GET /products/1/edit
-    def edit
-    end
-
-    # POST /products
-    # POST /products.json
-    #PAPERCLIP, added for: "params[:photo]"
-    def create
-      @product = Product.new(product_params, params[:photo] )
-      respond_to do |format|
-        if @product.save
-          format.html { redirect_to @product, notice: 'Product was successfully created.' }
-          format.json { render :show, status: :created, location: @product }
-        else
-          format.html { render :new }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+  # POST /products
+  # POST /products.json
+  #PAPERCLIP, added for: "params[:photo]"
+  def create
+    @product = Product.new(product_params, params[:photo] )
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    # PATCH/PUT /products/1
-    # PATCH/PUT /products/1.json
-    def update
-      respond_to do |format|
-        if @product.update(product_params)
-          format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-          format.json { render :show, status: :ok, location: @product }
-        else
-          format.html { render :edit }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
+  end
 
 
-    # DELETE /products/1
-    # DELETE /products/1.json
-    def destroy
-      @product.destroy
-      respond_to do |format|
-        format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
     end
-
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_product
-        @product = Product.find(params[:id])
-      end
+  end
 
 
-      # Never trust parameters from the scary internet, only allow the white list through.
-  # PAPERCLIP added :photo
-  # PRICE added :decimal
-      def product_params
-        params.require(:product).permit :name, :description, :image_url, :colour, :photo, :photo_file_name, :photo_file_size, :photo_content_type, :photo_updated_at, :price, :decimal, :precision, :scale
-      end
+
+
+  private
+
+
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+# PAPERCLIP added :photo
+# PRICE added :decimal
+  def product_params
+    params.require(:product).permit :name, :description, :image_url, :colour, :photo, :photo_file_name, :photo_file_size, :photo_content_type, :photo_updated_at, :price, :decimal, :precision, :scale
+  end
 
 end
