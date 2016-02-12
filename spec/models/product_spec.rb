87346bbf8eb03d697c7078a3d2e0fd73c185f67a
slaveunit <marco.abundo@live.com>
2016-02-12 02:04:06 +0000

    Rspec model test rating average.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 describe Product do
+
 	context "when the product has comments" do
+
 		before do
 			@product = Product.create!(name: "Skull Tee", description: "A t-shirt with a skull.", colour: "gray", price: "18")
 			@user = User.create!(first_name: "This", last_name: "Guy", email: "somekindof@email.com", password: "1qaz2wsx")
 			@product.comments.create!(rating: 1, user: @user, body: "Sick tee!")
 			@product.comments.create!(rating: 3, user: @user, body: "Nice!")
 			@product.comments.create!(rating: 5, user: @user, body: "Awesome!")
-			 
 		end
+
 		it "should return the average rating of comments" do
-			expect(@product.average_rating).to eq 3
+			expect(@product.average_rating).to eq 3		
 		end
+		it "is not valid" do
+			expect(Product.new(description: "Word.")).not_to be_valid	
+		end
+
 	end
 
 end
+
+
