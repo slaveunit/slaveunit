6a7e3cb5df1ec308586a0a7c6dcda65c9531317e
slaveunit <marco.abundo@live.com>
2016-02-25 23:02:29 +0000

    Attempt 5,097 fix cart + Stripe.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,23 +1,33 @@
 class CartsController < ApplicationController
 
 
-def show
-	@order_items = current_order.order_items # from SHOPPING CART
-	
 
-		# STRIPE CHECKOUT
-		@current_user ||= User.find(session[:user_id])
-		charge = Stripe::Charge.create(
-			:customer    => 'user',
-			:amount      => 'total_price',
-			:currency    => 'usd'
-		)
+	def show
+		@order_items = current_order.order_items # from SHOPPING CART
+		
 
-		rescue Stripe::CardError => e
-		flash[:error] = e.message
-		redirect_to new_charge_path
-		end
-		# STRIPE CHECKOUT
 
+
+
+			# STRIPE CHECKOUT
+
+			@unit_price = product.price
+			@total_price = unit_price * quantity
+
+
+			@current_user ||= User.find(session[:user_id])
+			charge = Stripe::Charge.create(
+				:customer    => 'current_user',
+				:amount      => 'total_price',
+				:currency    => 'usd'
+			)
+
+			rescue Stripe::CardError => e
+			flash[:error] = e.message
+			redirect_to new_charge_path
+			
+			# STRIPE CHECKOUT
+
+	end
 
 end
