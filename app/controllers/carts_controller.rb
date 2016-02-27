795498e14cc4dd18fab79db1c0e1ccf7a575bc32
slaveunit <marco.abundo@live.com>
2016-02-27 04:43:45 +0000

    Attempt fix Stripe + cart amount.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,33 +1,30 @@
 class CartsController < ApplicationController
 
-
-
 	def show
 		@order_items = current_order.order_items # from SHOPPING CART
-		
-
-
-
-
-			# STRIPE CHECKOUT
-
-			@unit_price = product.price
-			@total_price = unit_price * quantity
-
-
-			@current_user ||= User.find(session[:user_id])
-			charge = Stripe::Charge.create(
-				:customer    => 'current_user',
-				:amount      => 'total_price',
-				:currency    => 'usd'
-			)
-
-			rescue Stripe::CardError => e
-			flash[:error] = e.message
-			redirect_to new_charge_path
-			
-			# STRIPE CHECKOUT
-
 	end
+
+# STRIPE, instead of generating a controller, I put this block.
+	def create
+		# Amount in cents
+		@amount = amount
+
+		customer = Stripe::Customer.create(
+		:email => params[:stripeEmail],
+		:source  => params[:stripeToken]
+		)
+
+		charge = Stripe::Charge.create(
+		:customer    => customer.id,
+		:amount      => @amount,
+		:description => 'Rails Stripe customer',
+		:currency    => 'usd'
+		)
+
+		rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to new_charge_path
+	end
+# STRIPE, instead of generating a controller, I put this block.
 
 end
